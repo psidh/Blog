3a91541a5d34a627aa011be9f0d26bc98e908cbc
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-11-28 23:41:02 +0530

    philkhana sidharth is the title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,10 @@ export default function Navbar() {
     <div className="p-6">
       <div>
         <div>
-          <h1 className="text-3xl hidden md:flex font-bold text-left">
-            Sidharth's Blog
+          <h1 className="text-3xl hidden md:flex font-bold text-left my-2">
+            Philkhana Sidharth
           </h1>
-          <h2>Student | Developer </h2>
+          <h2 className='my-2 text-[#c2c2c2]'>Student | Developer </h2>
         
           <div className="flex justify-center items-center my-4 space-x-4">
             <a href="/" className="nav-link">

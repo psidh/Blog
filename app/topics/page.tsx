38462aacafb9e2b3fb0e9e3611842f8b3553edec
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-03-26 11:42:09 +0530

    topics added

--- app/topics/page.tsx
+++ app/topics/page.tsx
@@ -2,13 +2,27 @@ export default function page() {
   return (
     <>
       <div className='flex flex-col items-center justify-center'>
-        <div className='w-[60%]'>
-          <h1>Web</h1>
-          <h1>Cloud</h1>
-          <h1>DevOps</h1>
-          <h1>Front End</h1>
-          <h1>Back End</h1>
-          <h1>Full Stack</h1>
+        <div className='w-[80%]'>
+          <div className='topic'>
+            <img src='/blog1.png' alt='topic images' className='topic-img' />
+            <h1 className='topic-title'>Web</h1>
+          </div>
+          <div className='topic'>
+            <img src='/blog3.png' alt='topic images' className='topic-img' />
+            <h1 className='topic-title'>Cloud</h1>
+          </div>
+          <div className='topic'>
+            <img src='/blog2.png' alt='topic images' className='topic-img' />
+            <h1 className='topic-title'>DSA</h1>
+          </div>
+          <div className='topic'>
+            <img src='/blog2.png' alt='topic images' className='topic-img' />
+            <h1 className='topic-title'>Front End</h1>
+          </div>
+          <div className='topic'>
+            <img src='/blog2.png' alt='topic images' className='topic-img' />
+            <h1 className='topic-title'>Web</h1>
+          </div>
         </div>
       </div>
     </>

9dc2c5fb2d77f37ca10c6daaba8198c820010ec9
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-04-21 18:04:05 +0530

    chnages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,24 @@
 export default function Navbar() {
   return (
-    <div className="p-6">
+    <div className='p-6'>
       <div>
         <div>
-          <h1 className="text-3xl text-fuchsia-400  font-bold text-left my-2">
+          <h1 className='text-3xl text-green-400  font-bold text-left my-2'>
             Philkhana Sidharth
           </h1>
-          <h2 className="my-2 text-[#c2c2c2]">Student | Developer </h2>
+          <h2 className='my-2 text-[#c2c2c2]'>Student | Developer </h2>
 
-          <div className="flex justify-center items-center my-4 space-x-4">
-            <a href="/" className="nav-link">
+          <div className='flex justify-center items-center my-4 space-x-4'>
+            <a href='/' className='nav-link'>
               Home
             </a>
-            <a
-              href="/topics"
-              className="nav-link"
-            >
+            <a href='/topics' className='nav-link'>
               Topics
             </a>
             <a
-              href="https://psidh.vercel.app"
-              target="Portfolio"
-              className="nav-link"
+              href='https://psidh.vercel.app'
+              target='Portfolio'
+              className='nav-link'
             >
               Portfolio
             </a>
